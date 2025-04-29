ca61d07001eb20235128db6d7f40bd21db84b294
AtharvaWandhare <atharvawandhare@gmail.com>
2025-04-30 00:13:38 +0530

    Add DocumentEditor component and update Home page with Create Document button

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -6,7 +6,7 @@ export default function Home() {
 
       <div className="flex flex-col items-center justify-start min-h-screen py-2 border">
         <h1>Welcome to My Website</h1>
-
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Create a Document</button>
       </div>
 
     </>
