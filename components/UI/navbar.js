b27cb662f22c5caaf1969f3967d52a4151384b37
AtharvaWandhare <atharvawandhare@gmail.com>
2025-05-01 20:04:01 +0530

    Refactor Navbar component to include user authentication and logout functionality

--- components/UI/navbar.js
+++ components/UI/navbar.js
@@ -1,12 +1,22 @@
+'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation'
 import { APP_NAME, APP_DESCRIPTION } from '@/config/keys.js';
 import Image from 'next/image';
 import Logo from '@/public/logos/Center/logo-png.png';
-// import Logo from '@/public/logos/CenLeft_Stacked/logo-png.png';
+import { useUser } from '@/context/context.js';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export default function Navbar() {
 
-    const loggedIn = false;
+    const { user, logout } = useUser();
+    const router = useRouter();
+
+    const redirect = (path) => {
+        router.push(path);
+    }
 
     const logo = <Image
         src={Logo}
@@ -17,6 +27,27 @@ export default function Navbar() {
         quality={100}
         width={60}
     />
+
+    async function handleClick() {
+        if (user) {
+            const token = Cookies.get('AuthToken');
+            await axios.post('https://localhost:8000/api/v1/user/logout',
+                { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': `Bearer ${token}` }, withCredentials: true })
+                .then((response) => {
+                    if (response.status === 200) {
+                        console.log('Logout successful:', response.data);
+                        Cookies.remove('AuthToken', { path: '/' });
+                        logout();
+                        setLoggedIn(false);
+                    } else {
+                        console.log('Logout failed:', response.data);
+                    }
+                }).catch((error) => {
+                    console.log('Error during logout:', error);
+                });
+        }
+        redirect('/login');
+    }
 
     return (
         <nav className="text-white p-4 border">
@@ -36,14 +67,14 @@ export default function Navbar() {
                         <Link href="/" className="hover:text-gray-400">Home</Link>
                     </li>
                     <li>
-                        <Link href="/documents" className="hover:text-gray-400">Documents</Link>
+                        <Link href="/document" className="hover:text-gray-400">Documents</Link>
                     </li>
                     <li>
-                        <Link href="/about" className="hover:text-gray-400">Account</Link>
+                        <Link href="/account" className="hover:text-gray-400">Account</Link>
                     </li>
                     <li>
-                        <button className="hover:text-gray-400 bg-amber-50 text-black font-bold py-1 px-2 rounded-lg">
-                            {loggedIn ? 'Logout' : 'Login'}
+                        <button onClick={handleClick} className="hover:text-gray-400 bg-amber-50 text-black font-bold py-1 px-2 rounded-lg cursor-pointer">
+                            {user ? 'Logout' : 'Login'}
                         </button>
                     </li>
                 </ul>
